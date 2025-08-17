aa6288de82ad34839e9ef67532270fe68d96931d
Siddharth M Chauhan <github.chauhan.siddharth@gmail.com>
2025-08-16 22:58:47 -0700

    fix: update __init__.py to match README

--- src/nbmf_mm/__init__.py
+++ src/nbmf_mm/__init__.py
@@ -1,11 +1,13 @@
 """
-NBMF-MM: Bernoulli Mean-Parameterized Non-negative Binary Matrix Factorization with Majorization-Minimization
+NBMF-MM: Bernoulli Mean-Parameterized Non-negative Binary Matrix Factorization with Majorization–Minimization
 
 Public API for nbmf_mm.
-A scikit-learn compatible implementation of Bernoulli mean-parameterized 
-Non-negative Binary Matrix Factorization using Majorization-Minimization.
-"""
-from ._version import __version__
-from .estimator import BernoulliNMF_MM
 
-__all__ = ["BernoulliNMF_MM", "__version__"]
+A scikit-learn compatible implementation of Bernoulli mean-parameterized
+Non-negative Binary Matrix Factorization using Majorization–Minimization.
+"""
+
+from ._version import __version__
+from .estimator import BernoulliNMF_MM as NBMF
+
+__all__ = ["NBMF", "__version__"]
